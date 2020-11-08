dfd140c1fb877548a13e03c84c2560334bd2b5db
Paul Boehringer <paulboehringer0989@gmail.com>
2020-11-08 15:08:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# SimpleYelp
+
+## *Paul Boehringer*
+
+**Simple_Yelp** integrates with the yelp API to get relecant information on popular resturants that have avacado toast in New York City. 
+
+Time spent: **5** hours spent in total
+
+## Functionality 
+
+The following **extensions** are implemented:
+
+* [x] Check for internet connectivity
+
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://imgur.com/a/w0XwysR' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [shareX](http://www.shareX.com/).
+
+## Notes
+
+Overall a fun app to build. Though kotlin is still new to me I am beginning to appriciate the differences between it an python. It is more "strict" but thanks to this seems to provide more seamless ability to control user interaction. 
+
+## License
+
+    Copyright 2020 Paul Boehringer
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.Init Git project on local end with readme
---
+++ app/src/main/java/com/example/simpleyelp/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.rkpandey.simpleyelp
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.simpleyelp.R
+import com.example.simpleyelp.YelpRestaurant
+import com.example.simpleyelp.YelpSearchResult
+import com.example.simpleyelp.YelpService
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.system.exitProcess
+
+private const val TAG = "MainActivity"
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val API_KEY = "-U1bfntUuMZ-vKhJTY-Se2qgA85B5bl22meeTZcL0QP53prEaE3lgO2RW5Rg9cbrl19_pHdhrDkvdmDzm37Nk0U1atjEGWVHXn_1Uk7t1mI5lqb36b_c7ro0gZImXnYx"
+class MainActivity : AppCompatActivity() {
+
+    fun getConnectionType(context: Context): Int {
+        var result = 0 // Returns connection type. 0: none; 1: mobile data; 2: wifi
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cm?.run {
+                cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+                    if (hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        result = 2
+                    } else if (hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        result = 1
+                    } else if (hasTransport(NetworkCapabilities.TRANSPORT_VPN)){
+                        result = 3
+                    }
+                }
+            }
+        } else {
+            cm?.run {
+                cm.activeNetworkInfo?.run {
+                    if (type == ConnectivityManager.TYPE_WIFI) {
+                        result = 2
+                    } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                        result = 1
+                    } else if(type == ConnectivityManager.TYPE_VPN) {
+                        result = 3
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+    val network_connection = getConnectionType(this)
+
+
+    fun quitAppIfNoNetwork(network_connection: Int) {
+        if (network_connection == 0) {
+            this@MainActivity.finish()
+            exitProcess(0)
+        }
+
+        return
+    }
+
+    val nothing = quitAppIfNoNetwork(network_connection)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val restaurants = mutableListOf<YelpRestaurant>()
+        val adapter = RestaurantsAdapter(this, restaurants)
+        rvRestaurants.adapter = adapter
+        rvRestaurants.layoutManager = LinearLayoutManager(this)
+
+        val retrofit =
+            Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+                .build()
+        val yelpService = retrofit.create(YelpService::class.java)
+        yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object : Callback<YelpSearchResult> {
+            override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
+                Log.i(TAG, "onResponse $response")
+                val body = response.body()
+                if (body == null) {
+                    Log.w(TAG, "Did not receive valid response body from Yelp API... exiting")
+                    return
+                }
+                restaurants.addAll(body.restaurants)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
+                Log.i(TAG, "onFailure $t")
+            }
+        })
+    }
+}
